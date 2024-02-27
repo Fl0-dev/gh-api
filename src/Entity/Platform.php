b5518e70645894a9a890045991b88d151d708d01
Fl0-dev <florian@lamacompta.co>
2024-02-27 12:32:29 +0100

    feat: vichUploader init and add logo on platform entity

--- src/Entity/Platform.php
+++ src/Entity/Platform.php
@@ -9,10 +9,34 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use Symfony\Component\Uid\Uuid;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: PlatformRepository::class)]
-#[ApiResource]
+#[Vich\Uploadable]
+#[ApiResource(
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(),
+        new Put(),
+        new Patch(),
+    ],
+    normalizationContext: [
+        'groups' => ['platform:read'],
+    ],
+    denormalizationContext: [
+        'groups' => ['platform:write'],
+    ]
+
+)]
 class Platform
 {
     #[ORM\Id]
@@ -23,13 +47,24 @@ class Platform
     private Uuid $id;
 
     #[ORM\Column(length: 100)]
+    #[Groups(['platform:read', 'platform:write'])]
     private ?string $name = null;
 
     #[ORM\Column(length: 100)]
+    #[Groups(['platform:read', 'platform:write'])]
     private ?string $slug = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
+    #[Groups(['platform:read', 'platform:write'])]
     private ?string $description = null;
+
+    #[ORM\Column(type: Types::STRING, length: 255, nullable: true)]
+    #[Groups(['platform:read'])]
+    private ?string $logoPath = null;
+
+    #[Vich\UploadableField(mapping: 'platform_logo', fileNameProperty: 'logoPath')]
+    #[Groups(['platform:write'])]
+    private ?File $logoFile = null;
 
     #[ORM\ManyToMany(targetEntity: Game::class, inversedBy: 'platforms')]
     private Collection $games;
@@ -78,6 +113,33 @@ class Platform
         $this->description = $description;
 
         return $this;
+    }
+
+    public function getLogoPath(?string $logoPath): ?string
+    {
+        return $this->logoPath;
+    }
+
+    public function setLogoPath(?string $logoPath): static
+    {
+        $this->logoPath = $logoPath;
+
+        return $this;
+    }
+
+    public function getLogoFile(): ?File
+    {
+        // TODO: Implement getLogoFile() method.
+        return $this->logoFile;
+    }
+
+    public function setLogoFile(?File $image): void
+    {
+        $this->logoFile = $image;
+
+        if ($image) {
+            // TODO: set logoPath with name of file
+        }
     }
 
     /**
